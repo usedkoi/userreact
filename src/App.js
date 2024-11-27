68bc00d7e826fe119d7e55da01661c8259dd7091
usedkoi <usedkoi@ajou.ac.kr>
2024-11-27 13:55:13 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/boards')
+      .then(response => {
+        setBoards(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the boards!', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Boards</h1>
+      <ul>
+        {boards.map(board => (
+          <li key={board.id}>
+            <p>Name: {board.name}</p>
+            <p>Address: {board.addr}</p>
+            <p>Birthday: {board.birthday}</p>
+            <p>Mobile: {board.mobile}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
